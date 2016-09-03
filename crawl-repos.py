2dc3a425861a2e2cf39d945128724e5be2ec2f1f
Wu Yuhao <wyhfrank@gmail.com>
2016-09-03 17:36:52 -0400

    Refactor: make the way of updating values in db more flexible.

--- crawl-repos.py
+++ crawl-repos.py
@@ -6,6 +6,8 @@ import re
 import os
 import argparse
 from pygithub3 import Github, exceptions as GitHubErrors
+import requests
+from bs4 import BeautifulSoup
 
 from mydb import MyDB
 
@@ -15,79 +17,174 @@ from crawl import src_dir_name, db_dir_name, db_file_name, mk_dirs, get_json_fro
 
 # repo_api = 'https://api.github.com/repos/{user_project}'
 # contrib_api = 'https://api.github.com/repos/{user_project}/contributors'
+url_template = 'https://github.com/{user}/{repo}'
 config_file = 'config.json'
 config = json.load(open(config_file))
+
+
+class GitHubCrawler(object):
+    def __init__(self, rows, callback, login=None, password=None):
+        self.login = login
+        self.password = password
+        self.callback = callback
+        self.rows = rows
+
+    def set_rows(self, rows):
+        self.rows = rows
+
+    def start(self, force=False):
+        gh = Github()
+        gh.repos.set_credentials(login=self.login, password=self.password)
+
+        total_count = len(self.rows)
+        count = 0
+        for row in self.rows:
+            raw_url = row['url']
+            # print raw_url
+            username, reponame = self.parse_user_repo(raw_url)
+            count += 1
+            if not (username and reponame):
+                print "[{current}/{total}]: illegal url: {url}" \
+                    .format(current=count, total=total_count, url=raw_url)
+                continue
+
+            print "[{current}/{total}]: repo:{user}/{repo}" \
+                .format(current=count, total=total_count, user=username, repo=reponame)
+
+            key1 = 'stars'
+            try:
+                old_stars = row[key1]
+                # print old_stars
+                if self.need_to_update_value(old_stars, force):
+                    try:
+                        repo_data = gh.repos.get(user=username, repo=reponame)
+                        tmp = self.retrieve_repo_basic_info(repo_data)
+                        row.update(tmp)
+                    except GitHubErrors.NotFound as e:
+                        print "Repo not found: [{u}/{r}]".format(u=username, r=reponame)
+                        row[key1] = -1
+                    except requests.exceptions.ConnectionError as e:
+                        print "Cannot connect to repo: {user}/{repo}".format(user=username, repo=reponame)
+            except (KeyError, IndexError) as e:
+                print "Warning: Key '{key}' not in data: {data}".format(key=key1, data=row)
+
+            key2 = 'commits'
+            try:
+                old_commits = row[key2]
+                if self.need_to_update_value(old_commits, force):
+                    repo_url = self.construct_repo_url(username, reponame)
+                    try:
+                        commits, contribs = self.parse_commits_contribs(repo_url)
+                        tmp = self.retrieve_repo_commit_info(commits, contribs)
+                        row.update(tmp)
+                    except IOError as e:
+                        print "Cannot connect to {url}.".format(url=repo_url)
+            except (KeyError, IndexError) as e:
+                print "Warning: Key {key} not in data: {data}".format(key=key2, data=row)
+            self.callback(row)
+
+    @staticmethod
+    def need_to_update_value(val, force, failed_val=-1):
+        if force:
+            return True
+
+        if val is None:
+            # val has not been processed yet
+            return True
+        elif val == failed_val:
+            # failed last time
+            return False
+        else:
+            # has a valid value (may be out-dated, though)
+            return False
+
+    @staticmethod
+    def parse_user_repo(url):
+        if url.find('github.com') != -1:
+            if url.endswith('.git'):
+                url = url[:-len('.git')]
+            pat = re.compile('github\.com/([^/]+)/([^/]+)')
+            match = pat.search(url)
+            if match:
+                return match.group(1), match.group(2)
+        return None, None
+
+    @staticmethod
+    def construct_repo_url(user, repo):
+        return url_template.format(user=user, repo=repo)
+
+    @staticmethod
+    def retrieve_repo_basic_info(repo_data):
+        info = dict(
+            stars=repo_data.stargazers_count,
+            size=repo_data.size,
+            watchers=repo_data.subscribers_count,
+            forks=repo_data.forks_count,
+            isfork=repo_data.fork,
+        )
+        return info
+
+    @staticmethod
+    def retrieve_repo_commit_info(commits, contributors):
+        return dict(commits=commits, contributors=contributors)
+
+    # The method to calculate the number of commits and contributors is not accurate using pygtihub3,
+    # thus parse the repository page directly
+    @staticmethod
+    def parse_commits_contribs(repo_url):
+        # with open(r'test-data\repo.html') as f:
+        #     html = f.read()
+        fh = urllib.urlopen(repo_url)
+        html = fh.read()
+        parsed_html = BeautifulSoup(html, 'html.parser')
+        ul = parsed_html.body.find('ul', attrs={'class': 'numbers-summary'})
+        commits = contribs = -1
+        for i in ul.find_all('a'):
+            num, name = list(i.stripped_strings)
+            if name.find('commit') != -1:
+                commits = int(num.replace(',', ''))
+            elif name.find('contributor') != -1:
+                contribs = int(num.replace(',', ''))
+
+        return commits, contribs
+
 
 def main():
     args = parse_arg()
 
     outdir, src_path, db_path = mk_dirs(args.outdir)
     with MyDB(os.path.join(db_path, db_file_name)) as db:
-        gh = Github()
-        gh.repos.set_credentials(login=config['login'], password=config['password'])
-
         rows = db.select_all_repos()
-        total_count = len(rows)
-        count = 0
-        for rid, repo_url in rows:
-            count += 1
-            print "[{current}/{total}]: id:{rid}, url:{url}"\
-                .format(current=count, total=total_count, rid=rid, url=repo_url)
-            username, reponame = retrieve_user_project(repo_url)
-            if username is None:
-                continue
 
-            try:
-                info=dict()
-                repo_data = gh.repos.get(user=username, repo=reponame)
-                tmp = retrieve_repo_basic_info(repo_data)
-                info.update(tmp)
-
-                contrib_list = gh.repos.list_contributors(user=username, repo=reponame).all()
-                tmp = retrieve_repo_commit_info(contrib_list)
-                info.update(tmp)
-
-                info['rid'] = rid
-                db.update_repo(**info)
-            except GitHubErrors.NotFound as e:
-                print "Repo not found: [{u}/{r}]".format(u=username, r=reponame)
-                db.update_repo(rid, commits=-1)
-            # break
-
-def retrieve_repo_basic_info(repo_data):
-    info = dict(
-        stars=repo_data.stargazers_count,
-        size=repo_data.size,
-        watchers=repo_data.subscribers_count,
-        forks=repo_data.forks_count,
-        isfork=repo_data.fork,
-    )
-    return info
-
-def retrieve_repo_commit_info(contrib_list):
-    commits = 0
-    contributors = 0
-    for user in contrib_list:
-        commits += user.contributions
-        contributors += 1
-    info = dict(commits=commits, contributors=contributors)
-    return info
-
-def retrieve_user_project(url):
-    if url.find('github.com') != -1:
-        pat = re.compile('github\.com/([^/]+)/(.+)\.git')
-        match = pat.search(url)
-        if match:
-            return match.group(1), match.group(2)
-    return None, None
+        gc = GitHubCrawler(rows, lambda r: db.update_repo(**r), config['login'], config['password'])
+        gc.start(args.force)
 
 
 def parse_arg():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--outdir', dest='outdir', default='data',
+    parser.add_argument('-o', '--outdir', dest='outdir', default='data',
                         help='specify the output directory (default: data)')
+    parser.add_argument('-f', '--force-update', dest='force', default=False,
+                        help='force updating data', action='store_true')
     args = parser.parse_args()
     return args
+
+
+def _test():
+    def process_data(row):
+        print row
+
+    rows = [
+        {
+            'url': 'http://github.com/wyhfrank/ninka',
+            'rid': 1,
+            'stars': None,
+            'commits': None,
+        }
+    ]
+
+    gc = GitHubCrawler(rows, process_data, config['login'], config['password'])
+    gc.start()
 
 
 if __name__ == '__main__':
--- mydb.py
+++ mydb.py
@@ -4,12 +4,26 @@ import sqlite3
 class MyDB:
     """ DB manager"""
 
+    # TODO: encapsulate data into a class
+    repo_attributes = dict(commits='INT',
+                           contributors='INT',
+                           size='INT',
+                           stars='INT',
+                           watchers='INT',
+                           forks='INT',
+                           isfork='INT', )
+
     def __init__(self, filename='data.db'):
         self.file = filename
 
     def __enter__(self):
         self.conn = sqlite3.connect(self.file)
         self.c = self.conn.cursor()
+
+        # http://stackoverflow.com/questions/3300464/how-can-i-get-dict-from-sqlite-query/3300514#3300514
+        # https://docs.python.org/3/library/sqlite3.html#sqlite3.Row
+        self.c_repo = self.conn.cursor()
+        self.c_repo.row_factory = sqlite3.Row
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -18,6 +32,7 @@ class MyDB:
     def createdb(self):
         self.c.execute('CREATE TABLE IF NOT EXISTS files (id INTEGER PRIMARY KEY, '
                        'name TEXT, repo_id INT, lang TEXT, url TEXT, hash TEXT, loc INT, location TEXT);')
+        # TODO: construct stmt using repo_attributes
         self.c.execute('CREATE TABLE IF NOT EXISTS repos (url TEXT PRIMARY KEY, name TEXT, commits INT, '
                        'contributors INT, size INT, stars INT, watchers INT, forks INT, isfork INT);')
         self.c.execute('CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY, type INT);')
@@ -44,45 +59,69 @@ class MyDB:
 
         self.conn.commit()
 
-    def select_all_repos(self, unprocessed_only=True):
-        stmt = 'SELECT oid, url FROM repos ORDER BY oid;'
-        if unprocessed_only:
-            stmt = 'SELECT oid, url FROM repos WHERE commits IS NULL ORDER BY oid;'
-        self.c.execute(stmt)
-        return self.c.fetchall()
+    def select_all_repos(self):
+        props = ', '.join(self.repo_attributes.keys())
+        stmt = 'SELECT oid as rid, url, {0} FROM repos ORDER BY oid;'.format(props)
+        self.c_repo.execute(stmt)
+        return self.c_repo.fetchall()
 
-    def update_repo(self, rid, commits=None, contributors=None, size=None,
-                    stars=None, watchers=None, forks=None, isfork=None):
-        if commits is None:
-            return
+    def update_repo(self, rid, **kwargs):
+        class SqlHelper:
+            stmt = 'UPDATE repos SET {values} WHERE oid=?;'
 
-        if commits == -1:
-            self.c.execute('UPDATE repos SET commits =? WHERE oid=?;', (commits, rid))
-            self.conn.commit()
-            return
+            def __init__(self, rid):
+                self.cols = []
+                self.vals = [rid]
 
-        isfork_int = 0
-        if isfork:
-            isfork_int = 1
+            def append_sql_arg(self, key, val):
+                self.cols.insert(0, '{key}=?'.format(key=key))
+                self.vals.insert(0, val)
 
-        self.c.execute('UPDATE repos SET commits =?, contributors =?, size=?, '
-                       'stars=?, watchers=?, forks=?, isfork=? WHERE oid=?;', (commits,
-                       contributors, size, stars, watchers, forks, isfork_int, rid))
+            def get_stmt(self):
+                return self.stmt.format(values=', '.join(self.cols))
+
+            def get_args(self):
+                return self.vals
+
+        # TODO: do nothing if there's no update in the data
+        helper = SqlHelper(rid)
+        for kw in kwargs.iteritems():
+            if kw[0] in self.repo_attributes:
+                val = kw[1]
+                if val is None:
+                    continue
+                if val is bool:
+                    val = int(val)
+                helper.append_sql_arg(kw[0], val)
+            elif kw[0] in ('url', 'rid'):
+                pass
+            else:
+                raise KeyError("key not expected: {0}".format(kw[0]))
+
+        # print(helper.get_stmt(), helper.get_args())
+        self.c.execute(helper.get_stmt(), helper.get_args())
         self.conn.commit()
+
 
 def _test_create(fn):
     with MyDB(fn) as db:
         db.createdb()
+
 
 def _test_insert_file(fn):
     with MyDB(fn) as db:
         db.insertfile(123, 'file-a', 'test-repo')
         db.insertfile(556, 'file-b', 'test-repo')
 
+
 def _test_update_repo(fn):
     with MyDB(fn) as db:
-        for rid, url in db.select_all_repos():
-            db.update_repo(rid, -1)
+        for row in db.select_all_repos():
+            info = dict(row)
+            info['commits'] = 5
+            info['stars'] = 11
+            db.update_repo(**info)
+            # db.update_repo(rid, commits=2, stars=10, fff=0, isfork=False)
 
 
 if __name__ == '__main__':
